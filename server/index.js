b67eaddc0391884a68b576b514676c92f7c6c696
Daryl Marco <darmarco@gmail.com>
2019-10-23 00:16:10 -0700

    Set up the review and similar sections

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 let app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 3005;
+const url = require('url');
 
-app.use(bodyParser.json());
+app.use(cors({
+  credentials: true,
+}));
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// app.get('/api-reviews', function(req, res) {
+//   res.redirect(url.format({
+//     hostname: 'localhost',
+//     port: 3003,
+//     query: req.query,
+//     json: {msg: 'This is CORS-enabled for a Single Route'}
+//     })
+//   )
+// });
 
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}, ${new Date()}`);
